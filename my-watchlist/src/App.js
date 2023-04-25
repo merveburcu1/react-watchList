dd85819a16160a828bec3c0f006c16b1867474ff
merveburcu1 <101812503+merveburcu1@users.noreply.github.com>
2023-04-25 16:42:02 +0300

    react-watchList

---
+++ my-watchlist/src/App.js
@@ -0,0 +1,26 @@
+import Header from './components/Header'
+import './App.css';
+import './lib/fontawesome/css/all.min.css';
+import { BrowserRouter as Router ,Routes,Route } from 'react-router-dom';
+import Watchlist from './components/Watchlist';
+import Watched from './components/Watched';
+import Add from './components/Add';
+import { GlobalProvider } from './context/GlobalState';
+
+
+function App() {
+  return (
+    <GlobalProvider>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route  path="/" element={<Watchlist/>}/>     
+        <Route  path="/watched" element={<Watched/>}/>
+        <Route  path="/add" element={<Add/>}/>
+      </Routes>
+    </Router>
+    </GlobalProvider>
+  );
+}
+
+export default App;
